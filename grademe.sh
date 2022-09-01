d77b8ac7219b33d8f18054377de3d6838691f2ae
gkintana <81775867+gkintana@users.noreply.github.com>
2022-09-01 22:44:26 +0400

    refactor script

--- grademe.sh
+++ grademe.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# colors codes
+#######################################
+#             Definitions             #
+#######################################
+
 DEFAULT='\033[0m'
 RED='\033[1;31m'
 GREEN='\033[1;32m'
@@ -8,85 +11,60 @@ YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
 CYAN='\033[1;4;36m'
 
-# file definitions
-FT=ft_containers
-STD=std_containers
-FT_EXEC=./$FT
-FT_TEXT=$FT.txt
-STD_EXEC=./$STD
-STD_TEXT=$STD.txt
-RESULT=diff_result.txt
+OBJ_DIR=objects
+EXEC_DIR=executables
 
-# checks if the project has been compiled, if not, will call "re" rule
-# from makefile
-# if [ ! -f $FT ] && [ ! -f $STD ]; then
-	make re
-# fi
-
-# redirects output of ./ft_containers to ft_containers.txt
-
-# echo -e $PURPLE"$FT_EXEC > $FT_TEXT"$DEFAULT
-# $FT_EXEC > $FT_TEXT
-
-# redirects output of ./std_containers to std_containers.txt
-
-# echo -e $PURPLE"$STD_EXEC > $STD_TEXT"$DEFAULT
-# $STD_EXEC > $STD_TEXT
-
-# search for any differences between the ft and std containers with the
-# assistance of the diff command
-
-# echo -e $PURPLE"diff $FT_TEXT $STD_TEXT > $RESULT"$DEFAULT
-# diff $FT_TEXT $STD_TEXT > $RESULT
-
-# checks if diff_result.txt contains any information, and if they do,
-# print [KO]. But if they're empty, print [OK]
-
-# echo -e $YELLOW"Checking contents of $RESULT"$DEFAULT
-# if [ -s $RESULT ]; then
-# 	echo -e "Status: "$RED"[KO]"$DEFAULT
-# else
-# 	echo -e "Status: "$GREEN"[OK]"$DEFAULT
-# fi
-
-COMPILER=c++
-COMPILE_FLAGS='-Wall -Wextra -Werror'
+CC=c++
+CFLAGS='-Wall -Wextra -Werror'
 VALGRIND=valgrind
-VALGRIND_FLAGS='--leak-check=full --show-leak-kinds=all'
-EXEC=a.out
-TEXT_FILE=output.txt
+VFLAGS='--leak-check=full --show-leak-kinds=all'
+RM='rm -rf'
 
-for file in objects/*.o; do
+
+#######################################
+#             Compilation             #
+#######################################
+
+make re
+
+mkdir -p $EXEC_DIR
+for file in $OBJ_DIR/*.o; do
+	$CC $CFLAGS "$file" -o "${file%%.o}"
+	mv -- "${file%%.o}" $EXEC_DIR
+done
+
+
+#######################################
+#               Testing               #
+#######################################
+
+for file in $EXEC_DIR/*; do
     echo -e $CYAN"Testing $file"$DEFAULT
-	$COMPILER $COMPILE_FLAGS "$file"; ./$EXEC
+	./$file
 
 	if [[ "$OSTYPE" =~ ^linux ]]; then
-		$VALGRIND $VALGRIND_FLAGS ./$EXEC > $TEXT_FILE 2>&1
+		$VALGRIND $VFLAGS ./$file > $file.txt 2>&1
 		echo -e $CYAN"Valgrind report for $file"$DEFAULT
-		if grep -q "All heap blocks were freed -- no leaks are possible" $TEXT_FILE; then
+		if grep -q "All heap blocks were freed -- no leaks are possible" $file.txt; then
 			echo -e "Leaks: "$GREEN"[OK]"$DEFAULT
 		else
 			echo -e "Leaks: "$RED"[KO]"$DEFAULT
 		fi
 
-		if grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" $TEXT_FILE; then
+		if grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" $file.txt; then
 			echo -e "Error Summary: "$GREEN"[OK]\n"$DEFAULT
 		else
 			echo -e "Error Summary: "$RED"[KO]\n"$DEFAULT
 		fi
 	fi
-# echo "Output file => ${f%.*}"
 done
 
 
-# deletes all text files
+#######################################
+#               Cleanup               #
+#######################################
 
-# echo -e $PURPLE"Deleted $FT_TEXT, $STD_TEXT, and $RESULT"$DEFAULT
-# rm $FT_TEXT $STD_TEXT $RESULT
-# rm a.out vector_construction.txt
-if [[ "$OSTYPE" =~ ^linux ]]; then
-	rm a.out *.txt
-else
-	rm a.out
-fi
+printf "\033[A"
+
+$RM $EXEC_DIR
 make fclean
