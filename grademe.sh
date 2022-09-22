9515ec1c58bbb2b370f32281a3fdefb7575d047b
gkintana <81775867+gkintana@users.noreply.github.com>
2022-09-22 23:32:46 +0400

    refactor grademe.sh

--- grademe.sh
+++ grademe.sh
@@ -32,16 +32,9 @@ RM='rm -rf'
 
 
 #######################################
-#             Compilation             #
+#              Functions              #
 #######################################
-
-if [ $1 == "vector" ]; then
-	echo -e $CYAN"Vector Tester"$DEFAULT
-	$RM $EXEC_DIR
-	make fclean && make vector
-
-	mkdir -p $EXEC_DIR
-	mkdir -p $EXEC_DIR/$VEC_DIR
+compile_vector_tests() {
 	for file in $OBJ_DIR/$VEC_DIR/*.o; do
 		$CC $CFLAGS "$file" -o "${file%%.o}"
 		mv -- "${file%%.o}" $EXEC_DIR/$VEC_DIR
@@ -56,45 +49,60 @@ if [ $1 == "vector" ]; then
 		$CC $CFLAGS "$file" -o "${file%%.o}_std"
 		mv -- "${file%%.o}_std" $EXEC_DIR/$VEC_DIR
 	done
+}
 
-	# mkdir -p $EXEC_DIR/$FT_VEC
-	# for file in $OBJ_DIR/$FT_VEC/*.o; do
-	# 	$CC $CFLAGS "$file" -o "${file%%.o}_ft"
-	# 	mv -- "${file%%.o}_ft" $EXEC_DIR/$FT_VEC
-	# done
+print_vector_test_type() {
+	if [[ "${file: -3}" != "_ft" && "${file: -4}" != "_std" ]]; then
+		echo -e $CYAN"\nTesting $file"$DEFAULT
+	elif [[ "${file: -3}" == "_ft" ]]; then
+		echo -ne $YELLOW"ft::vector Time:\t\t"$DEFAULT
+	elif [[ "${file: -4}" == "_std" ]]; then
+		echo -ne $YELLOW"std::vector Time:\t\t"$DEFAULT
+	fi
+}
 
-	# mkdir -p $EXEC_DIR/$STD_VEC
-	# for file in $OBJ_DIR/$STD_VEC/*.o; do
-	# 	$CC $CFLAGS "$file" -o "${file%%.o}_std"
-	# 	mv -- "${file%%.o}_std" $EXEC_DIR/$STD_VEC
-	# done
-
+print_vector_test_results() {
 	for file in $EXEC_DIR/$VEC_DIR/*; do
-		if [[ "${file: -3}" != "_ft" && "${file: -4}" != "_std" ]]; then
-			echo -e $CYAN"\nTesting $file"$DEFAULT
-		elif [[ "${file: -3}" == "_ft" ]]; then
-			echo -ne $YELLOW"ft::vector Time:\t\t"$DEFAULT
-		elif [[ "${file: -4}" == "_std" ]]; then
-			echo -ne $YELLOW"std::vector Time:\t\t"$DEFAULT
-		fi
+		print_vector_test_type
 		./$file
-
-		if [[ "$OSTYPE" =~ ^linux && "${file: -3}" != "_ft" && "${file: -4}" != "_std" ]]; then
-			$VALGRIND $VFLAGS ./$file > $file.txt 2>&1
-			echo -e $CYAN"Valgrind report for $file"$DEFAULT
-			if grep -q "All heap blocks were freed -- no leaks are possible" $file.txt; then
-				echo -e "Leaks: "$GREEN"OK"$DEFAULT
-			else
-				echo -e "Leaks: "$RED"KO"$DEFAULT
-			fi
-
-			if grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" $file.txt; then
-				echo -e "Error Summary: "$GREEN"OK"$DEFAULT
-			else
-				echo -e "Error Summary: "$RED"KO"$DEFAULT
-			fi
-		fi
+		check_valgrind_report
 	done
+}
+
+check_valgrind_report() {
+	if [[ "$OSTYPE" =~ ^linux && "${file: -3}" != "_ft" && "${file: -4}" != "_std" ]]; then
+		$VALGRIND $VFLAGS ./$file > $file.txt 2>&1
+		echo -e $CYAN"Valgrind report for $file"$DEFAULT
+		if grep -q "All heap blocks were freed -- no leaks are possible" $file.txt; then
+			echo -e "Leaks: "$GREEN"OK"$DEFAULT
+		else
+			echo -e "Leaks: "$RED"KO"$DEFAULT
+		fi
+
+		if grep -q "ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)" $file.txt; then
+			echo -e "Error Summary: "$GREEN"OK"$DEFAULT
+		else
+			echo -e "Error Summary: "$RED"KO"$DEFAULT
+		fi
+	fi
+}
+
+
+#######################################
+#             Compilation             #
+#######################################
+
+$RM $EXEC_DIR
+
+if [ $1 == "vector" ]; then
+	echo -e $CYAN"Vector Tester"$DEFAULT
+
+	make fclean && make vector
+	mkdir -p $EXEC_DIR
+	mkdir -p $EXEC_DIR/$VEC_DIR
+
+	compile_vector_tests
+	print_vector_test_results
 
 elif [ $1 == "stack" ]; then
 	echo -e $CYAN"Stack Tester"$DEFAULT
