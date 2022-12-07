41318192aa5b59907e22a7bf6f150bd231e3e47a
gkintana <81775867+gkintana@users.noreply.github.com>
2022-12-08 00:45:13 +0400

    update map::swap test

--- sources/map_tests/swap.cpp
+++ sources/map_tests/swap.cpp
@@ -6,7 +6,7 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 12:17:20 by gkintana          #+#    #+#             */
-/*   Updated: 2022/12/07 16:59:27 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/12/08 00:44:17 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int main() {
 
 	{
 		NAMESPACE::map<int, int> map_1;
+		NAMESPACE::map<int, int>::iterator it_begin_1 = map_1.begin();
+		NAMESPACE::map<int, int>::iterator it_end_1 = map_1.end();
 		insertValue(map_1, INSERT_BY_OPERATOR, 9040, -1143);
 		insertValue(map_1, INSERT_BY_OPERATOR, 19340, 18827);
 		insertValue(map_1, INSERT_BY_OPERATOR, -11346, -19857);
@@ -28,14 +30,36 @@ int main() {
 		printContent(map_1);
 
 		NAMESPACE::map<int, int> map_2;
+		NAMESPACE::map<int, int>::iterator it_begin_2 = map_2.begin();
+		NAMESPACE::map<int, int>::iterator it_end_2 = map_2.end();
 		insertValue(map_2, INSERT_BY_OPERATOR, -8101, 12848);
 		insertValue(map_2, INSERT_BY_OPERATOR, 11129, 10015);
 		insertValue(map_2, INSERT_BY_OPERATOR, 9366, 3005);
 		printContent(map_2);
 
+		std::cout << std::boolalpha
+		          << "Is it_begin_1 == it_begin_2? " << (it_begin_1 == it_begin_2) << std::endl
+		          << "Is it_end_1 == it_end_2? " << (it_end_1 == it_end_2) << std::endl
+		          << "Is map_1.begin() == it_begin_1? " << (map_1.begin() == it_begin_1) << std::endl
+		          << "Is map_1.end() == it_end_1? " << (map_1.end() == it_end_1) << std::endl;
+
+		it_begin_1 = map_1.begin();
+		it_end_1 = map_1.end();
+		it_begin_2 = map_2.begin();
+		it_end_2 = map_2.end();
+
 		map_1.swap(map_2);
 		printContent(map_1);
 		printContent(map_2);
+
+		std::cout << "Is map_1.begin() == it_begin_1? " << (map_1.begin() == it_begin_1) << std::endl
+		          << "Is map_1.begin() == it_begin_2? " << (map_1.begin() == it_begin_2) << std::endl
+		          << "Is map_1.end() == it_end_1? " << (map_1.end() == it_end_1) << std::endl
+		          << "Is map_1.end() == it_end_2? " << (map_1.end() == it_end_2) << std::endl
+		          << "Is map_2.begin() == it_begin_1? " << (map_2.begin() == it_begin_1) << std::endl
+		          << "Is map_2.begin() == it_begin_2? " << (map_2.begin() == it_begin_2) << std::endl
+		          << "Is map_2.end() == it_end_1? " << (map_2.end() == it_end_1) << std::endl
+		          << "Is map_2.end() == it_end_2? " << (map_2.end() == it_end_2) << std::endl;
 
 		NAMESPACE::map<int, int> map_3(map_2);
 		map_3.swap(map_1);
