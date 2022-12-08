42e902dd099440e681efeb27929f2563c9a4972e
gkintana <81775867+gkintana@users.noreply.github.com>
2022-12-08 10:24:46 +0400

    update map::swap tests

--- sources/map_tests/swap.cpp
+++ sources/map_tests/swap.cpp
@@ -6,7 +6,7 @@
 /*   By: gkintana <gkintana@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 12:17:20 by gkintana          #+#    #+#             */
-/*   Updated: 2022/12/08 00:44:17 by gkintana         ###   ########.fr       */
+/*   Updated: 2022/12/08 10:22:44 by gkintana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,9 @@ int main() {
 		          << "Is it_begin_1 == it_begin_2? " << (it_begin_1 == it_begin_2) << std::endl
 		          << "Is it_end_1 == it_end_2? " << (it_end_1 == it_end_2) << std::endl
 		          << "Is map_1.begin() == it_begin_1? " << (map_1.begin() == it_begin_1) << std::endl
-		          << "Is map_1.end() == it_end_1? " << (map_1.end() == it_end_1) << std::endl;
+		          << "Is map_1.end() == it_end_1? " << (map_1.end() == it_end_1) << std::endl
+		          << "Is map_2.begin() == it_begin_2? " << (map_2.begin() == it_begin_2) << std::endl
+		          << "Is map_2.end() == it_end_2? " << (map_2.end() == it_end_2) << std::endl;
 
 		it_begin_1 = map_1.begin();
 		it_end_1 = map_1.end();
@@ -49,26 +51,29 @@ int main() {
 		it_end_2 = map_2.end();
 
 		map_1.swap(map_2);
-		printContent(map_1);
-		printContent(map_2);
+		printByIterator(map_1, it_begin_2, map_1.end());
+		printByIterator(map_2, it_begin_1, map_2.end());
 
 		std::cout << "Is map_1.begin() == it_begin_1? " << (map_1.begin() == it_begin_1) << std::endl
 		          << "Is map_1.begin() == it_begin_2? " << (map_1.begin() == it_begin_2) << std::endl
-		          << "Is map_1.end() == it_end_1? " << (map_1.end() == it_end_1) << std::endl
-		          << "Is map_1.end() == it_end_2? " << (map_1.end() == it_end_2) << std::endl
 		          << "Is map_2.begin() == it_begin_1? " << (map_2.begin() == it_begin_1) << std::endl
-		          << "Is map_2.begin() == it_begin_2? " << (map_2.begin() == it_begin_2) << std::endl
-		          << "Is map_2.end() == it_end_1? " << (map_2.end() == it_end_1) << std::endl
-		          << "Is map_2.end() == it_end_2? " << (map_2.end() == it_end_2) << std::endl;
+		          << "Is map_2.begin() == it_begin_2? " << (map_2.begin() == it_begin_2) << std::endl;
 
 		NAMESPACE::map<int, int> map_3(map_2);
+		NAMESPACE::map<int, int>::iterator it_begin_3 = map_3.begin();
+
+		std::cout << "Is map_3.begin() == it_begin_1? " << (map_3.begin() == it_begin_1) << std::endl
+		          << "Is map_3.end() == it_end_1? " << (map_3.end() == it_end_1) << std::endl
+		          << "Is map_3.begin() == it_begin_2? " << (map_3.begin() == it_begin_2) << std::endl
+		          << "Is map_3.end() == it_end_2? " << (map_3.end() == it_end_2) << std::endl;
+
 		map_3.swap(map_1);
-		printContent(map_1);
-		printContent(map_3);
+		printByIterator(map_1, it_begin_3, map_1.end());
+		printByIterator(map_3, it_begin_2, map_3.end());
 
 		map_3.swap(map_2);
-		printContent(map_2);
-		printContent(map_3);
+		printByIterator(map_2, it_begin_2, map_2.end());
+		printByIterator(map_3, it_begin_1, map_3.end());
 
 		map_3.clear();
 		printContent(map_1);
@@ -78,6 +83,8 @@ int main() {
 
 	{
 		NAMESPACE::map<char, std::string> map_1;
+		NAMESPACE::map<char, std::string>::iterator it_begin_1 = map_1.begin();
+		NAMESPACE::map<char, std::string>::iterator it_end_1 = map_1.end();
 		insertValue(map_1, INSERT_BY_MAKE_PAIR, 'b', "I admire your courage but I don't admire your singing ability.");
 		insertValue(map_1, INSERT_BY_MAKE_PAIR, '0', "There was a big earthquake last night.");
 		insertValue(map_1, INSERT_BY_MAKE_PAIR, ' ', "Tom can't remember all his passwords, so he keeps them in a list disguised as phone numbers.");
@@ -87,19 +94,39 @@ int main() {
 		insertValue(map_1, INSERT_BY_MAKE_PAIR, '^', "Have you heard the news?");
 
 		NAMESPACE::map<char, std::string> map_2;
+		NAMESPACE::map<char, std::string>::iterator it_begin_2 = map_2.begin();
+		NAMESPACE::map<char, std::string>::iterator it_end_2 = map_2.end();
 		insertValue(map_2, INSERT_BY_MAKE_PAIR, 'G', "Lorem ipsum dolor sit amet");
 		insertValue(map_2, INSERT_BY_MAKE_PAIR, '@', "");
 		insertValue(map_2, INSERT_BY_MAKE_PAIR, '\'', "The quick brown fox jumps over the lazy dog");
-		
+
+		std::cout << "Is it_begin_1 == it_begin_2? " << (it_begin_1 == it_begin_2) << std::endl
+		          << "Is it_end_1 == it_end_2? " << (it_end_1 == it_end_2) << std::endl
+		          << "Is map_1.begin() == it_begin_1? " << (map_1.begin() == it_begin_1) << std::endl
+		          << "Is map_1.end() == it_end_1? " << (map_1.end() == it_end_1) << std::endl
+		          << "Is map_2.begin() == it_begin_2? " << (map_2.begin() == it_begin_2) << std::endl
+		          << "Is map_2.end() == it_end_2? " << (map_2.end() == it_end_2) << std::endl;
+
+		it_begin_1 = map_1.begin();
+		it_end_1 = map_1.end();
+		it_begin_2 = map_2.begin();
+		it_end_2 = map_2.end();
 
 		map_1.swap(map_2);
-		printContent(map_1);
-		printContent(map_2);
+		printByIterator(map_1, it_begin_2, map_1.end());
+		printByIterator(map_2, it_begin_1, map_2.end());
 
 		NAMESPACE::map<char, std::string> map_3(map_1);
+		NAMESPACE::map<char, std::string>::iterator it_begin_3 = map_3.begin();
+
+		std::cout << "Is map_3.begin() == it_begin_1? " << (map_3.begin() == it_begin_1) << std::endl
+		          << "Is map_3.end() == it_end_1? " << (map_3.end() == it_end_1) << std::endl
+		          << "Is map_3.begin() == it_begin_2? " << (map_3.begin() == it_begin_2) << std::endl
+		          << "Is map_3.end() == it_end_2? " << (map_3.end() == it_end_2) << std::endl;
+
 		map_3.swap(map_2);
-		printContent(map_2);
-		printContent(map_3);
+		printByIterator(map_2, it_begin_3, map_2.end());
+		printByIterator(map_3, it_begin_1, map_3.end());
 
 		map_3.clear();
 		printContent(map_1);
