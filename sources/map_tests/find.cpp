a3fb622eb2cb0c454c34fae4563c7d3d5d1ec315
gkintana <81775867+gkintana@users.noreply.github.com>
2023-04-07 00:09:52 +0400

    update map::find test & add temporary thread

--- sources/map_tests/find.cpp
+++ sources/map_tests/find.cpp
@@ -12,29 +12,87 @@
 
 #include "map_templates.hpp"
 
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+#define TIMEOUT 12
+#ifndef ENABLE_THREAD
+# define ENABLE_THREAD false
+#endif
+
+void* timer_thread(void* arg)
+{
+	(void)arg;
+    time_t start_time = time(NULL);
+    while (true) {
+        time_t current_time = time(NULL);
+        if (difftime(current_time, start_time) > TIMEOUT) {
+            std::cout << "TIMEOUT" << std::endl;
+            exit(1);
+        }
+        usleep(100);
+    }
+}
+
 int main() {
 	timeval exec_time;
 	gettimeofday(&exec_time, NULL);
 	double start = 1.0e6 * exec_time.tv_sec + exec_time.tv_usec;
 
-	NAMESPACE::map<int, int> test;
-	NAMESPACE::map<int, int>::iterator it = test.end();
-	insertRandomValues(test, 20);
-	printContent(test);
-
-	for (NAMESPACE::map<int, int>::iterator it = test.begin(); it != test.end(); it++) {
-		std::cout << "map::find(" << it->first << ") = " << test.find(it->first)->first << std::endl;
+	if (ENABLE_THREAD == true) {
+		pthread_t timer;
+		if (pthread_create(&timer, NULL, timer_thread, NULL)) {
+			std::cerr << "Error creating thread.\n";
+			return 1;
+		}
+		if (pthread_detach(timer)) {
+			std::cerr << "Error detaching thread.\n";
+			return 1;
+		}
 	}
 
-	NAMESPACE::map<int, int>::key_type value = (++(++test.begin()))->first;
-	std::cout << "Is map::find("<< value << ") == map::end()? "
-	          << std::boolalpha << (test.find(value) == test.end()) << std::endl;
+	{
+		NAMESPACE::map<int, int> test;
+		NAMESPACE::map<int, int>::iterator it = test.end();
+		insertRandomValues(test, 20);
+		printContent(test);
 
-	value = 42;
-	std::cout << "Is map::find("<< value << ") == map::end()? "
-	          << (test.find(value) == test.end()) << std::endl
-	          << "Is map::find("<< value << ") == it? "
-	          << (test.find(value) == it) << std::endl;
+		for (NAMESPACE::map<int, int>::iterator it = test.begin(); it != test.end(); it++) {
+			std::cout << "map::find(" << it->first << ") = " << test.find(it->first)->first << std::endl;
+		}
+
+		NAMESPACE::map<int, int>::key_type value = (++(++test.begin()))->first;
+		std::cout << "Is map::find("<< value << ") == map::end()? "
+		          << std::boolalpha << (test.find(value) == test.end()) << std::endl;
+
+		value = 42;
+		std::cout << "Is map::find("<< value << ") == map::end()? "
+		          << (test.find(value) == test.end()) << std::endl
+		          << "Is map::find("<< value << ") == it? "
+		          << (test.find(value) == it) << std::endl;
+	}
+
+	{
+		size_t sample_size = 2500000;
+		NAMESPACE::map<int, int> test;
+		for (size_t i = 0; i <= sample_size; i++) {
+			test.insert(NAMESPACE::make_pair(i, i * 2));
+		}
+
+		NAMESPACE::map<int, int>::key_type value = sample_size * 0.50;
+		std::cout << "map::find("<< value << ")->first = " << test.find(value)->first << std::endl
+		          << "map::find("<< value << ")->second = " << test.find(value)->second << std::endl;
+
+		value = sample_size * 0.85;
+		std::cout << "map::find("<< value << ")->first = " << test.find(value)->first << std::endl
+		          << "map::find("<< value << ")->second = " << test.find(value)->second << std::endl;
+
+		value = sample_size;
+		std::cout << "map::find("<< value << ")->first = " << test.find(value)->first << std::endl
+		          << "map::find("<< value << ")->second = " << test.find(value)->second << std::endl;
+	}
 
 	gettimeofday(&exec_time, NULL);
 	double end = 1.0e6 * exec_time.tv_sec + exec_time.tv_usec;
